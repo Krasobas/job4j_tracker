7c27351291edf06501b975268589d29e8460739c
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-02-16 06:40:45 +0300

    1.2. Вызов метода объекта [#404484]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Student {
+
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        for (int index = 0; index < 6; index++) {
+            if (index < 3) {
+                petya.music();
+            } else {
+                petya.song();
+            }
+        }
+    }
+}
