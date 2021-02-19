652e006c73d7cf2c950cd19bdb433fca3898e6df
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-02-19 17:27:04 +0300

    6. Метод замены заявки. Tracker.replace [#404506]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,6 +13,16 @@ public class Tracker {
         return item;
     }
 
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            items[index].setId(id);
+            return true;
+        }
+        return false;
+    }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
@@ -29,15 +39,19 @@ public class Tracker {
         return Arrays.copyOf(keys, length);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 }
