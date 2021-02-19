819f3d6b8831fc5528b2b9ee6de0e43aa2112153
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-02-19 07:56:42 +0300

    3. Массивы и модели. [#404503] (добавил метод swapBook())

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -1,21 +1,27 @@
 package ru.job4j.pojo;
 
 public class Library {
+
+    public static Book[] swapBooks(Book[] books, int first, int second) {
+        Book temp;
+        temp = books[first];
+        books[first] = books[second];
+        books[second] = temp;
+        return books;
+    }
+
     public static void main(String[] args) {
         Book[] books = {new Book("Java - The Complete Reference", 1248),
                         new Book("Head First Java", 720),
                         new Book("Head First Design Patterns", 694),
                         new Book("Clean code", 464)};
-        Book temp;
         System.out.println("\nLibrary before replacement:");
         for (int index = 0; index < books.length; index++) {
             System.out.println(books[index].getName() + " - "
                         + books[index].getPages() + " pages.");
         }
         System.out.println("\nLibrary after replacement:");
-        temp = books[0];
-        books[0] = books[3];
-        books[3] = temp;
+        books = Library.swapBooks(books, 0, 3);
         for (int index = 0; index < books.length; index++) {
             System.out.println(books[index].getName() + " - "
                     + books[index].getPages() + " pages.");
