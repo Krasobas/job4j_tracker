568eb2d9a8c997c0386d61ad6f9e99ad5cda946d
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-02-23 13:32:10 +0300

    4.2. Статические методы. [#404438]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,70 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void showAll(Tracker tracker) {
+        System.out.println("=== All Items of the Tracker ====");
+        Item[] all = tracker.findAll();
+        if (all.length > 0) {
+            for (Item item : all) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("There is no any Item in the Tracker.");
+        }
+    }
+
+    public static void replaceItem(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ====");
+        int id = input.askInt("Enter Id of Item you would like to edit: ");
+        String name = input.askStr("Enter new name: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Item was successfully edited!");
+        } else {
+            System.out.println("Something went wrong, try again!");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("=== Delete item ====");
+        int id = input.askInt("Enter Id of Item you would like to delete: ");
+        if (tracker.delete(id)) {
+            System.out.println("Item was successfully deleted!");
+        } else {
+            System.out.println("Something went wrong, try again!");
+        }
+    }
+
+    public static void searchById(Input input, Tracker tracker) {
+        System.out.println("=== Find item by Id ====");
+        int id = input.askInt("Enter Id of Item you would like to find: ");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("There is no any Item with this Id in the Tracker.");
+        }
+    }
+
+    public static void searchByName(Input input, Tracker tracker) {
+        System.out.println("=== Find items by name ====");
+        String name = input.askStr("Enter the name of Items you would like to find: ");
+        Item[] items = tracker.findByName(name);
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("There is no any Item with this name in the Tracker.");
+        }
+    }
 
     public void init(Input input, Tracker tracker) {
         boolean run = true;
@@ -8,58 +72,17 @@ public class StartUI {
             this.showMenu();
             int select = input.askInt("Select: ");
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("=== All Items of the Tracker ====");
-                Item[] all = tracker.findAll();
-                if (all.length > 0) {
-                    for (Item item : all) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.println("There is no any Item in the Tracker.");
-                }
+                StartUI.showAll(tracker);
             } else if (select == 2) {
-                System.out.println("=== Edit item ====");
-                int id = input.askInt("Enter Id of Item you would like to edit: ");
-                String name = input.askStr("Enter new name: ");
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Item was successfully edited!");
-                } else {
-                    System.out.println("Something went wrong, try again!");
-                }
+                StartUI.replaceItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("=== Delete item ====");
-                int id = input.askInt("Enter Id of Item you would like to delete: ");
-                if (tracker.delete(id)) {
-                    System.out.println("Item was successfully deleted!");
-                } else {
-                    System.out.println("Something went wrong, try again!");
-                }
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
-                System.out.println("=== Find item by Id ====");
-                int id = input.askInt("Enter Id of Item you would like to find: ");
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    System.out.println(item);
-                } else {
-                    System.out.println("There is no any Item with this Id in the Tracker.");
-                }
+                StartUI.searchById(input, tracker);
             } else if (select == 5) {
-                System.out.println("=== Find items by name ====");
-                String name = input.askStr("Enter the name of Items you would like to find: ");
-                Item[] items = tracker.findByName(name);
-                if (items.length > 0) {
-                    for (Item item : items) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.println("There is no any Item with this name in the Tracker.");
-                }
+                StartUI.searchByName(input, tracker);
             } else if (select == 6) {
                 System.out.println("Goodbye!");
                 run = false;
